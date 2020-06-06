b8f1a16cfed0119239c39206a83c4b89c54402dd
GakkyTom <itagaki.tomoya@eversense.co.jp>
2020-06-06 21:59:03 +0900

    Initial Commit

---
+++ TinderUIPoC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderUIPoC
+//
+//  Created by 板垣智也 on 2020/06/06.
+//  Copyright © 2020 板垣智也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
